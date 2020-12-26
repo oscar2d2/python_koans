4cf77c1986f0f4a679c104f55650a32582604287
oscar2d2 <oscarkong@gmail.com>
2020-12-26 13:35:23 +0000

    triangle part 2

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -17,6 +17,12 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0:
+        raise TriangleError
+
+    if a + b <= c or b + c <= a or c + a <= b:
+        raise TriangleError
+
     if a == b and b == c:
         return 'equilateral'
     elif a != b and b != c and c != a:
